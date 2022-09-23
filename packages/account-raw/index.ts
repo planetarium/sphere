dec938de10fe4061f54e6736dab0e1b318540ac0
Basix <basix@basix.tech>
2022-09-23 18:52:33 +0900

    Add getAddress implementation

--- packages/account-raw/index.ts
+++ packages/account-raw/index.ts
@@ -1,22 +1,27 @@
 import type { Account } from "@planetarium/sign";
 import * as secp from "@noble/secp256k1";
+import { keccak_256 } from "@noble/hashes/sha3";
 
 export function createAccount(privateKey?: string | Uint8Array): Account {
-  const privKeyBuf =
+  const privKey =
     typeof privateKey === "string"
       ? secp.utils.hexToBytes(privateKey)
       : privateKey ?? secp.utils.randomPrivateKey();
   return {
     VERSION: 0,
     getAddress() {
-      return Promise.reject(new Error("not impl"));
+      return Promise.resolve(
+        secp.utils.bytesToHex(
+          keccak_256(secp.getPublicKey(privKey, false).slice(1))
+        )
+      );
     },
     getPublicKey() {
-      return Promise.resolve(secp.getPublicKey(privKeyBuf));
+      return Promise.resolve(secp.getPublicKey(privKey));
     },
     sign(hash) {
       return secp
-        .sign(new Uint8Array(hash), privKeyBuf)
+        .sign(new Uint8Array(hash), privKey)
         .then((array) => array.buffer);
     },
   };
