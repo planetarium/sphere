3a282d88787513881552f44753bc109ddf00263c
Basix <basix@basix.tech>
2022-09-26 15:22:58 +0900

    Implement EIP-55 compatible address checksum

--- packages/account-raw/index.ts
+++ packages/account-raw/index.ts
@@ -2,6 +2,16 @@ import type { Account } from "@planetarium/sign";
 import * as secp from "@noble/secp256k1";
 import { keccak_256 } from "@noble/hashes/sha3";
 
+// Source: https://github.com/VictorTaelin/eth-lib/blob/da0971f5b09964d9c8449975fa87933f0c9fef35/src/account.js#L15
+function toChecksum(address: string) {
+  const addressHash = secp.utils.bytesToHex(keccak_256(address));
+  let checksumAddress = "";
+  for (let i = 0; i < address.length; i++)
+    checksumAddress +=
+      parseInt(addressHash[i], 16) > 7 ? address[i].toUpperCase() : address[i];
+  return checksumAddress;
+}
+
 export function createAccount(privateKey?: string | Uint8Array): Account {
   const privKey =
     typeof privateKey === "string"
@@ -11,9 +21,14 @@ export function createAccount(privateKey?: string | Uint8Array): Account {
     VERSION: 0,
     getAddress() {
       return Promise.resolve(
-        secp.utils.bytesToHex(
-          keccak_256(secp.getPublicKey(privKey, false).slice(1))
-        )
+        "0x" +
+          toChecksum(
+            secp.utils
+              .bytesToHex(
+                keccak_256(secp.getPublicKey(privKey, false).slice(1))
+              )
+              .slice(-40)
+          )
       );
     },
     getPublicKey() {
