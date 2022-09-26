b9f90451a505e46a300981ed0a5e456717e48c26
Akamig <akamig@posteo.net>
2022-09-26 17:17:26 +0900

    listAccounts Implemented

--- packages/account-local/index.ts
+++ packages/account-local/index.ts
@@ -1,13 +1,34 @@
 import { Account } from "../sign";
 import { KEYSTORE_PATH } from "./util";
 import fs from "fs";
-import { homedir } from "os";
 import path from "path";
 
-export class LocalKeyStore(path = KEYSTORE_PATH[process.platform]) {
-    
-    function listAccounts(path: string): string[]{
-        return fs.readdirSync(path).filter(f => f.match(/^UTC--\d{4}-\d\d-\d\dT\d\d-\d\d-\d\dZ--([\da-f]{8}-?(?:[\da-f]{4}-?){3}[\da-f]{12})$/i));
-    }
+export function listAccounts(
+  folder: string | undefined = KEYSTORE_PATH[process.platform]
+): string[] {
+  if (typeof folder === "string" && !fs.existsSync(folder)) {
+    throw new Error("This path does not exists.");
+  } else if (typeof folder !== "string") {
+    throw new Error("Invalid path value.");
+  }
+  const list = fs
+    .readdirSync(folder)
+    .filter((f) =>
+      f.match(
+        /^UTC--\d{4}-\d\d-\d\dT\d\d-\d\d-\d\dZ--([\da-f]{8}-?(?:[\da-f]{4}-?){3}[\da-f]{12})$/i
+      )
+    )
+    .map((file) => {
+      return path.resolve(folder, file);
+    });
+  if (list.length <= 0) {
+    throw new Error("No keys found in folder");
+  }
+  return list;
 }
 
+export function createAccount(folder?: string): Account {
+  return {
+    VERSION: 0,
+  };
+}
