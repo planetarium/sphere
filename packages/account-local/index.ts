ccb715e5410de2e5769854a7e2f39bda482fff9c
moreal <dev.moreal@gmail.com>
2023-02-16 15:19:43 +0900

    Replace ehterumjs-wallet with ethers.js

--- packages/account-local/index.ts
+++ packages/account-local/index.ts
@@ -4,6 +4,7 @@ import { decipherV3, V3Keystore, rawPrivateKeyToV3 } from "./v3";
 import fs from "fs/promises";
 import path from "path";
 import * as secp from "@noble/secp256k1";
+import ethers from "ethers";
 
 /**
  * account-local
@@ -48,9 +49,8 @@ export async function getAccountFromV3(
     resolve({
       VERSION: 0,
       getPublicKey(isCompressed: boolean = true) {
-        const publicKey = new Uint8Array(
-          decipherV3(V3Keystore, passphrase).getPublicKey()
-        );
+        const signingKey = new ethers.SigningKey(decipherV3(V3Keystore, passphrase).privateKey.substring(2));
+        const publicKey = new Uint8Array(Buffer.from(signingKey.publicKey.substring(2), "hex"));
         if (isCompressed)
           return Promise.resolve(
             secp.utils.concatBytes(
@@ -65,7 +65,7 @@ export async function getAccountFromV3(
       sign(hash) {
         return secp.sign(
           hash,
-          decipherV3(V3Keystore, passphrase).getPrivateKey()
+          decipherV3(V3Keystore, passphrase).privateKey.substring(2)
         );
       },
     });
